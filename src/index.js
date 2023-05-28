fa714b3141bf2e8812bfc9bcdcda49a306a142ac
JuanEs02 <114095229+JuanEs02@users.noreply.github.com>
2023-05-28 15:38:07 -0500

    01

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Login from './components/login';
+import Register from './components/register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
