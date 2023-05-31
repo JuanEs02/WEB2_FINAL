b9482a70a7d721b640d4b4866a1652496de8f8f7
JuanEs02 <114095229+JuanEs02@users.noreply.github.com>
2023-05-30 19:00:04 -0500

    07

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -13,16 +13,29 @@ const menu = () => {
       <div className="menu">
         <nav>
           <h2>Menu de CRUDS</h2>
-          <Link to="/productos" style={{animationDelay: "0.2s"}}><strong>Productos</strong></Link>
-          <Link to="/clientes" style={{animationDelay: "0.3s"}}><strong>Clientes</strong></Link>
-          <Link to="/tareas" style={{animationDelay: "0.4s"}}><strong>Tareas</strong></Link>
-          <Link to="/publicaciones" style={{animationDelay: "0.5s"}}><strong>Publicaciones</strong></Link>
-          <Link to="/empleados" style={{animationDelay: "0.7s"}}><strong>Empleados</strong></Link>
+          <Link to="/productos" style={{ animationDelay: "0.2s" }}><strong>Productos</strong></Link>
+          <Link to="/clientes" style={{ animationDelay: "0.3s" }}><strong>Clientes</strong></Link>
+          <Link to="/tareas" style={{ animationDelay: "0.4s" }}><strong>Tareas</strong></Link>
+          <Link to="/publicaciones" style={{ animationDelay: "0.5s" }}><strong>Publicaciones</strong></Link>
+          <Link to="/empleados" style={{ animationDelay: "0.7s" }}><strong>Empleados</strong></Link>
         </nav>
       </div>
-      <h2>JUAN ESMERAL</h2>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-9">
+          </div>
+          <div class="col-md-3 dark_bar">
+            <h1>5 Cruds</h1>
+            <br />
+            <h1>Juan Esmeral</h1>
+            <br />
+            <div class="square"></div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
 export default menu
--- src/components/productos.jsx
+++ src/components/productos.jsx
@@ -1,90 +1,102 @@
-import React, { useState, useEffect } from 'react'
-import './assets/productos/productos.css'
+import React, { useState, useEffect } from 'react';
+import './assets/productos/productos.css';
 import { Link } from 'react-router-dom';
-import { db } from '../firebase'
-import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore'
+import { db } from '../firebase';
+import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore';
 
 const Productos = () => {
   const toggleMenu = () => {
-    document.body.classList.toggle("open");
+    document.body.classList.toggle('open');
   };
 
-  const [id, setId] = useState(0)
-  const [listaProductos, setListaProductos] = useState([])
-  const [modoEdicion, setModoEdicion] = useState(false)
+  const [id, setId] = useState(0);
+  const [listaProductos, setListaProductos] = useState([]);
+  const [modoEdicion, setModoEdicion] = useState(false);
 
-  const [nombre, setNombre] = useState('')
-  const [descripcion, setDescripcion] = useState('')
-  const [precio, setPrecio] = useState(0)
-  const [cantidad_disponible, setCantidadDisponible] = useState(0)
+  const [nombre, setNombre] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [precio, setPrecio] = useState(0);
+  const [cantidad_disponible, setCantidadDisponible] = useState(0);
+
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const obtenerDatos = async () => {
       try {
         await onSnapshot(collection(db, 'productos'), (query) => {
-          setListaProductos(query.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-        })
+          setListaProductos(
+            query.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+          );
+        });
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
+    };
     obtenerDatos();
-  }, [])
+  }, []);
 
-  const editar = item => {
-    setNombre(item.nombre)
-    setDescripcion(item.descripcion)
-    setPrecio(item.precio)
-    setCantidadDisponible(item.cantidad_disponible)
-    setId(item.id)
-    setModoEdicion(true)
-}
+  const editar = (item) => {
+    setNombre(item.nombre);
+    setDescripcion(item.descripcion);
+    setPrecio(item.precio);
+    setCantidadDisponible(item.cantidad_disponible);
+    setId(item.id);
+    setModoEdicion(true);
+  };
 
+  const editarProductos = async (e) => {
+    e.preventDefault();
 
-const editarProductos = async e => {
-  e.preventDefault();
+    if (!validarCampos()) {
+      return;
+    }
 
-  try {
+    try {
       const docRef = doc(db, 'productos', id);
       await updateDoc(docRef, {
         nombre: nombre,
         descripcion: descripcion,
         precio: precio,
         cantidad_disponible: cantidad_disponible
-      })
+      });
 
-      const nuevoArray = listaProductos.map(
-          item => item.id === id ? {
-              id: id,
-              nombre: nombre,
-              descripcion: descripcion,
-              precio: precio,
-              cantidad_disponible: cantidad_disponible
-          } : item
-      )
+      const nuevoArray = listaProductos.map((item) =>
+        item.id === id
+          ? {
+            id: id,
+            nombre: nombre,
+            descripcion: descripcion,
+            precio: precio,
+            cantidad_disponible: cantidad_disponible
+          }
+          : item
+      );
 
-      setListaProductos(nuevoArray)
-      setModoEdicion(false)
-      setNombre('')
-      setDescripcion('')
-      setPrecio('')
-      setCantidadDisponible('')
-      setId('')
-  } catch (error) {
-      console.log(error)
-  }
-}
+      setListaProductos(nuevoArray);
+      setModoEdicion(false);
+      setNombre('');
+      setDescripcion('');
+      setPrecio('');
+      setCantidadDisponible('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-const cancelar = () => {
-  setModoEdicion(false)
-  setNombre('')
-  setDescripcion('')
-  setPrecio('')
-  setCantidadDisponible('')
-}
+  const cancelar = () => {
+    setModoEdicion(false);
+    setNombre('');
+    setDescripcion('');
+    setPrecio('');
+    setCantidadDisponible('');
+  };
 
   const guardarProductos = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
+    if (!validarCampos()) {
+      return;
+    }
 
     try {
       const data = await addDoc(collection(db, 'productos'), {
@@ -92,32 +104,54 @@ const cancelar = () => {
         descripcion: descripcion,
         precio: precio,
         cantidad_disponible: cantidad_disponible
-      })
-      setListaProductos(
-        [...listaProductos, {
+      });
+      setListaProductos([
+        ...listaProductos,
+        {
           id: data.id,
           nombre: nombre,
           descripcion: descripcion,
           precio: precio,
           cantidad_disponible: cantidad_disponible
-        }]
-      )
-      setNombre('')
-      setDescripcion('')
-      setPrecio('')
-      setCantidadDisponible('')
+        }
+      ]);
+      setNombre('');
+      setDescripcion('');
+      setPrecio('');
+      setCantidadDisponible('');
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  const eliminar = async id => {
+  const eliminar = async (id) => {
     try {
-        await deleteDoc(doc(db, 'productos', id))
+      await deleteDoc(doc(db, 'productos', id));
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
+  };
+
+  const validarCampos = () => {
+    setError(null);
+
+    if (nombre.trim() === '' || descripcion.trim() === '') {
+      setError('Los campos de nombre y/o descripción no pueden estar vacíos.');
+      return false;
+    }
+
+    if (precio < 5000 || precio > 1000000 || isNaN(precio)) {
+      setError('El precio debe estar entre $5.000 y $1.000.000');
+      return false;
+    }
+
+    if (cantidad_disponible <= 0 || cantidad_disponible > 100 || isNaN(cantidad_disponible)) {
+      setError('La cantidad disponible debe ser mayor a 0 y no puede superar 100.');
+      return false;
+    }
+
+    return true;
+  };
 
   return (
     <div>
@@ -125,64 +159,167 @@ const cancelar = () => {
       <div className="menu closed">
         <nav>
           <h2>Menu de CRUDs</h2>
-          <Link to="/" style={{ animationDelay: "0.2s" }}><strong>Regresar</strong></Link>
+          <Link to="/" style={{ animationDelay: '0.2s' }}>
+            <strong>Regresar</strong>
+          </Link>
           <br />
           <h2>Otros CRUDs</h2>
-          <Link to="/clientes" style={{ animationDelay: "0.3s" }}><strong>Clientes</strong></Link>
-          <Link to="/tareas" style={{ animationDelay: "0.4s" }}><strong>Tareas</strong></Link>
-          <Link to="/publicaciones" style={{ animationDelay: "0.5s" }}><strong>Publicaciones</strong></Link>
-          <Link to="/empleados" style={{ animationDelay: "0.7s" }}><strong>Empleados</strong></Link>
+          <Link to="/clientes" style={{ animationDelay: '0.3s' }}>
+            <strong>Clientes</strong>
+          </Link>
+          <Link to="/tareas" style={{ animationDelay: '0.4s' }}>
+            <strong>Tareas</strong>
+          </Link>
+          <Link to="/publicaciones" style={{ animationDelay: '0.5s' }}>
+            <strong>Publicaciones</strong>
+          </Link>
+          <Link to="/empleados" style={{ animationDelay: '0.7s' }}>
+            <strong>Empleados</strong>
+          </Link>
         </nav>
       </div>
 
-      <div className='container mt-5'>
+      <div className="container mt-5">
         <h1 className="text-center text-light">Productos</h1>
         <hr />
         <div className="row">
           <div className="col-8">
             <h4 className="text-center text-light">Lista de Productos</h4>
             <ul className="list-group">
-              {
-                listaProductos.map(item => (
-                  <li className="list-group-item" key={item.id}>
-                    <span className="lead">
-                      Nombre: {item.nombre}<br>
-                      </br>Descripcion: {item.descripcion}<br>
-                      </br>Precio: {item.precio}<br>
-                      </br>Cantidad disponible: {item.cantidad_disponible}<br>
-                      </br>
-                    </span>
-                    <button className='btn btn-danger btn-sm fload-end mx-2' onClick={() => eliminar(item.id)}>Eliminar</button>
-                    <button className="btn btn-dark btn-sm fload-end" onClick={() => editar(item)}>Editar</button>
-                  </li>
-                ))
-              }
+              {listaProductos.map((item) => (
+                <li className="list-group-item" key={item.id}>
+                  <span className="lead">
+                    Nombre: {item.nombre}
+                    <br />
+                    Descripcion: {item.descripcion}
+                    <br />
+                    Precio: {item.precio}
+                    <br />
+                    Cantidad disponible: {item.cantidad_disponible}
+                    <br />
+                  </span>
+                  <button
+                    className="btn btn-danger btn-sm fload-end mx-2"
+                    onClick={() => eliminar(item.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <button
+                    className="btn btn-dark btn-sm fload-end"
+                    onClick={() => editar(item)}
+                  >
+                    Editar
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
           <div className="col-4">
-            <h4 className="text-center text-light">{modoEdicion ? 'Editar Producto' : 'Añadir Producto'}</h4>
+            <h4 className="text-center text-light">
+              {modoEdicion ? 'Editar Producto' : 'Añadir Producto'}
+            </h4>
             <form onSubmit={modoEdicion ? editarProductos : guardarProductos}>
-              <input type="text" className="form-control mb-2" placeholder="Ingrese un nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-              <input type="text" className="form-control mb-2" placeholder="Ingrese una descripcion" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
-              <input type="text" className="form-control mb-2" placeholder="Ingrese el precio" value={precio} onChange={(e) => setPrecio(e.target.value)} />
-              <input type="text" className="form-control mb-2" placeholder="Ingrese la cantidad" value={cantidad_disponible} onChange={(e) => setCantidadDisponible(e.target.value)} />
+              <div class="input-group mb-3">
+                <span class="input-group-text mb-2 material-symbols-outlined" id="basic-addon1"> border_color </span>
+                <input
+                  type="text"
+                  className="form-control mb-2"
+                  placeholder="Ingrese un nombre"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
+                />
+              </div>
+              <div class="input-group mb-3">
+              <span class="input-group-text mb-2 material-symbols-outlined" id="basic-addon1"> article </span>
+                <input
+                  type="text"
+                  className="form-control mb-2"
+                  placeholder="Ingrese una descripcion"
+                  value={descripcion}
+                  onChange={(e) => setDescripcion(e.target.value)}
+                />
+              </div>
+              <div class="input-group mb-3">
+              <span class="input-group-text mb-2 material-symbols-outlined" id="basic-addon1"> attach_money </span>
+                <input
+                  type="text"
+                  className="form-control mb-2"
+                  placeholder="Ingrese el precio"
+                  value={precio}
+                  onChange={(e) => setPrecio(e.target.value)}
+                />
+              </div>
+              <div class="input-group mb-3">
+              <span class="input-group-text mb-2 material-symbols-outlined" id="basic-addon1"> inventory_2 </span>
+                <input
+                  type="text"
+                  className="form-control mb-2"
+                  placeholder="Ingrese la cantidad"
+                  value={cantidad_disponible}
+                  onChange={(e) => setCantidadDisponible(e.target.value)}
+                />
+              </div>
               <div className="d-grid gap-2">
-                {
-                  modoEdicion ? (
-                    <>
-                      <button className="btn btn-dark col-12" on='submit'>Editar</button>
-                      <button className='btn btn-danger btn-block' onClick={() => cancelar()}>Cancelar</button>
-                    </>
-                  ) :
-                    <button className='btn btn-dark btn-block'>Agregar</button>
-                }
+                {modoEdicion ? (
+                  <>
+                    <button className="btn btn-dark col-12" type="submit">
+                      Editar
+                    </button>
+                    <button
+                      className="btn btn-danger btn-block"
+                      onClick={() => cancelar()}
+                    >
+                      Cancelar
+                    </button>
+                  </>
+                ) : (
+                  <button className="btn btn-dark btn-block">Agregar</button>
+                )}
               </div>
             </form>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
 
-export default Productos
+      {error && (
+        <div
+          className="modal fade show"
+          style={{ display: 'block' }}
+          tabIndex="-1"
+          role="dialog">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content bg-dark">
+              <div className="modal-header">
+                <h5 className="modal-title">Error al ingresar datos</h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={() => setError(null)}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <p>{error}</p>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  data-dismiss="modal"
+                  onClick={() => setError(null)}
+                >
+                  Cerrar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Productos;
